800d2cc31e43caa7add0d5ade3315c82fac23300
Sniadek (Personal) <karolgruszka@linux-1.home>
2020-08-01 13:42:04 +0200

    bootstrap the project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Requirements
+
+
+[Here](https://yew.rs/docs/getting-started/project-setup) are the required libraries and tools you're gonna need to start this project. Node and npm / yarn is also needed.
---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+#![recursion_limit = "512"]
+use conway::Grid;
+use draw::Renderer;
+use soundgen::SoundGenerator;
+use std::{thread, time};
+use wasm_bindgen::prelude::*;
+
+mod app;
+mod conway;
+mod draw;
+mod soundgen;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is the entry point for the web app
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<app::App>();
+
+    let mut grid = Grid::random();
+    let renderer = Renderer::new();
+    let soundgen = SoundGenerator::new();
+
+    loop {
+        renderer.draw(&grid).expect("Fix it");
+
+        thread::sleep(time::Duration::from_millis(100));
+
+        for organism in grid.organisms() {
+            if organism.is_dying() {
+                soundgen.play(organism.cell_count() as u32).expect("Fix it");
+            }
+        }
+
+        grid.next_gen();
+    }
+
+    Ok(())
+}
