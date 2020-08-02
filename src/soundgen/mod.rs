50af48585df3a8f865baf1616f0bc6aa69a3752f
Tomasz Kurcz <uint@lavabit.com>
2020-08-02 17:33:37 +0200

    Audio: refactor note gen and add a couple tests to validate the logic

--- src/soundgen/mod.rs
+++ src/soundgen/mod.rs
@@ -10,8 +10,7 @@ type Result<V> = std::result::Result<V, SoundError>;
 pub struct SoundGenerator {
     ctx: AudioContext,
     sound: Sound,
-    range: Range,
-    scale: Scale,
+    notegen: NoteGenerator,
 }
 
 impl SoundGenerator {
@@ -19,13 +18,23 @@ impl SoundGenerator {
         SoundGenerator {
             ctx: AudioContext::new().unwrap(),
             sound: Sound::staccato_sine(),
-            range: Range::new(3, 7),
-            scale: Scale::pentatonic(),
+            notegen: NoteGenerator::new(Range::new(3, 7), Scale::pentatonic()),
         }
     }
 
-    pub fn _set_sound(&mut self, sound: Sound) {
+    #[allow(dead_code)]
+    pub fn set_sound(&mut self, sound: Sound) {
         self.sound = sound;
+    }
+
+    #[allow(dead_code)]
+    pub fn set_range(&mut self, range: Range) {
+        self.notegen.range = range;
+    }
+
+    #[allow(dead_code)]
+    pub fn set_scale(&mut self, scale: Scale) {
+        self.notegen.scale = scale;
     }
 
     pub fn play(&self, value: u32) -> Result<()> {
@@ -39,7 +48,7 @@ impl SoundGenerator {
 
         // Some initial settings:
         primary.set_type(sound.oscillator_type);
-        primary.frequency().set_value(self.frequency_from_value(value));
+        primary.frequency().set_value(self.notegen.frequency_from_value(value));
 
         // Give the amp a shape.
         gain.gain().set_value(0.0);
@@ -78,23 +87,7 @@ impl SoundGenerator {
         Ok(())
     }
 
-    /// Given a u32 value, this will deterministically produce notes from this generator's scale
-    /// and range. The result is a midi note.
-    fn midi_note_from_value(&self, value: u32) -> u8 {
-        let divisor = self.scale.len();
 
-        // Choose a note from the scale.
-        let base = self.scale.0[value as usize % divisor] as u8;
-
-        // Figure out how many octaves we have to move it up.
-        let octave_shift = (value / divisor as u32) % self.range.count();
-
-        12 + base + ((self.range.lowest_octave as u32 + octave_shift) * 12) as u8
-    }
-
-    fn frequency_from_value(&self, value: u32) -> f32 {
-        midi_to_freq(self.midi_note_from_value(value))
-    }
 }
 
 pub struct Sound {
@@ -119,6 +112,48 @@ impl Sound {
             fm_gain: 0.0,
             fm_freq: 0.0,
         }
+    }
+}
+
+pub struct NoteGenerator {
+    range: Range,
+    scale: Scale,
+}
+
+impl NoteGenerator {
+    pub fn new(range: Range, scale: Scale) -> Self {
+        NoteGenerator {
+            range,
+            scale,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn set_range(&mut self, range: Range) {
+        self.range = range;
+    }
+
+    #[allow(dead_code)]
+    pub fn set_scale(&mut self, scale: Scale) {
+        self.scale = scale;
+    }
+
+    /// Given a u32 value, this will deterministically produce notes from this generator's scale
+    /// and range. The result is a midi note.
+    pub fn midi_note_from_value(&self, value: u32) -> u8 {
+        let divisor = self.scale.len();
+
+        // Choose a note from the scale.
+        let base = self.scale.0[value as usize % divisor] as u8;
+
+        // Figure out how many octaves we have to move it up.
+        let octave_shift = (value / divisor as u32) % self.range.count();
+
+        12 + base + ((self.range.lowest_octave as u32 + octave_shift) * 12) as u8
+    }
+
+    pub fn frequency_from_value(&self, value: u32) -> f32 {
+        midi_to_freq(self.midi_note_from_value(value))
     }
 }
 
@@ -158,7 +193,7 @@ enum Note {
     B  = 11,
 }
 
-struct Scale(Vec<Note>);
+pub struct Scale(Vec<Note>);
 
 impl Scale {
     pub fn pentatonic() -> Self {
@@ -194,4 +229,30 @@ impl From<JsValue> for SoundError {
         // TODO: Figure out how to represent this JS value in a "sendable" way.
         SoundError::WasmError
     }
+}
+
+#[test]
+fn note_generation_single_octave_pentatonic() {
+    let notegen = NoteGenerator::new(Range::new(1, 1), Scale::pentatonic());
+
+    assert_eq!(notegen.midi_note_from_value(0), 24);
+    assert_eq!(notegen.midi_note_from_value(1), 26);
+    assert_eq!(notegen.midi_note_from_value(2), 28);
+    assert_eq!(notegen.midi_note_from_value(3), 31);
+    assert_eq!(notegen.midi_note_from_value(4), 33);
+    assert_eq!(notegen.midi_note_from_value(5), 24);
+}
+
+#[test]
+fn note_generation_three_octave_pentatonic() {
+    let notegen = NoteGenerator::new(Range::new(2, 4), Scale::pentatonic());
+
+    assert_eq!(notegen.midi_note_from_value(1), 38);
+    assert_eq!(notegen.midi_note_from_value(2), 40);
+    assert_eq!(notegen.midi_note_from_value(3), 43);
+    assert_eq!(notegen.midi_note_from_value(4), 45);
+    assert_eq!(notegen.midi_note_from_value(5), 48);
+    assert_eq!(notegen.midi_note_from_value(8), 55);
+    assert_eq!(notegen.midi_note_from_value(13), 67);
+    assert_eq!(notegen.midi_note_from_value(18), 43);
 }
