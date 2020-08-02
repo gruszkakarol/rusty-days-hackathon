982a4409c0ec591f905ff7bbf23338af7e64df1f
Tomasz Kurcz <uint@lavabit.com>
2020-08-02 17:15:15 +0200

    Audio: More flexibility when chosing scales, ranges, wave shapes, and oscs

--- src/soundgen/mod.rs
+++ src/soundgen/mod.rs
@@ -7,23 +7,30 @@ use wasm_bindgen::JsValue;
 
 type Result<V> = std::result::Result<V, SoundError>;
 
-const ATTACK: f64 = 0.15;
-const RELEASE: f64 = 0.3;
-const SWEEP: f64 = 0.7;
-const PEAK: f32 = 0.6;
-
 pub struct SoundGenerator {
     ctx: AudioContext,
+    sound: Sound,
+    range: Range,
+    scale: Scale,
 }
 
 impl SoundGenerator {
     pub fn new() -> SoundGenerator {
         SoundGenerator {
             ctx: AudioContext::new().unwrap(),
+            sound: Sound::staccato_sine(),
+            range: Range::new(3, 7),
+            scale: Scale::pentatonic(),
         }
     }
 
+    pub fn _set_sound(&mut self, sound: Sound) {
+        self.sound = sound;
+    }
+
     pub fn play(&self, value: u32) -> Result<()> {
+        let sound = &self.sound;
+
         // Create our web audio objects.
         let primary = self.ctx.create_oscillator()?;
         let fm_osc = self.ctx.create_oscillator()?;
@@ -31,18 +38,18 @@ impl SoundGenerator {
         let fm_gain = self.ctx.create_gain()?;
 
         // Some initial settings:
-        primary.set_type(OscillatorType::Sine);
-        primary.frequency().set_value(pentatonic(value));
+        primary.set_type(sound.oscillator_type);
+        primary.frequency().set_value(self.frequency_from_value(value));
 
         // Give the amp a shape.
         gain.gain().set_value(0.0);
-        gain.gain().linear_ramp_to_value_at_time(PEAK, self.ctx.current_time() + ATTACK)?;
-        gain.gain().linear_ramp_to_value_at_time(0.0, self.ctx.current_time() + SWEEP - RELEASE)?;
+        gain.gain().linear_ramp_to_value_at_time(sound.peak, self.ctx.current_time() + sound.attack)?;
+        gain.gain().linear_ramp_to_value_at_time(0.0, self.ctx.current_time() + sound.sweep - sound.release)?;
         
         // The FM oscillator isn't really used right now.
-        fm_gain.gain().set_value(0.0); // no initial frequency modulation
+        fm_gain.gain().set_value(sound.fm_gain); // no initial frequency modulation
         fm_osc.set_type(OscillatorType::Sine);
-        fm_osc.frequency().set_value(0.0);
+        fm_osc.frequency().set_value(sound.fm_freq);
 
         // Connect the nodes up!
 
@@ -64,32 +71,115 @@ impl SoundGenerator {
 
         // Start the oscillators!
         primary.start()?;
-        primary.stop_with_when(self.ctx.current_time() + SWEEP)?;
+        primary.stop_with_when(self.ctx.current_time() + sound.sweep)?;
         fm_osc.start()?;
-        fm_osc.stop_with_when(self.ctx.current_time() + SWEEP)?;
+        fm_osc.stop_with_when(self.ctx.current_time() + sound.sweep)?;
 
         Ok(())
+    }
+
+    /// Given a u32 value, this will deterministically produce notes from this generator's scale
+    /// and range. The result is a midi note.
+    fn midi_note_from_value(&self, value: u32) -> u8 {
+        let divisor = self.scale.len();
+
+        // Choose a note from the scale.
+        let base = self.scale.0[value as usize % divisor] as u8;
+
+        // Figure out how many octaves we have to move it up.
+        let octave_shift = (value / divisor as u32) % self.range.count();
+
+        12 + base + ((self.range.lowest_octave as u32 + octave_shift) * 12) as u8
+    }
+
+    fn frequency_from_value(&self, value: u32) -> f32 {
+        midi_to_freq(self.midi_note_from_value(value))
+    }
+}
+
+pub struct Sound {
+    attack: f64,
+    release: f64,
+    sweep: f64,
+    peak: f32,
+    oscillator_type: OscillatorType,
+    fm_gain: f32,
+    fm_freq: f32,
+}
+
+impl Sound {
+    // Constructors
+    fn staccato_sine() -> Self {
+        Sound {
+            attack: 0.15,
+            release: 0.3,
+            sweep: 0.7,
+            peak: 0.6,
+            oscillator_type: OscillatorType::Sine,
+            fm_gain: 0.0,
+            fm_freq: 0.0,
+        }
+    }
+}
+
+// Valid octaves: 1-8 - these are the octaves from music theory
+pub struct Range {
+    lowest_octave: u8,
+    highest_octave: u8,
+}
+
+impl Range {
+    pub fn new(lowest_octave: u8, highest_octave: u8) -> Self {
+        Range {
+            lowest_octave,
+            highest_octave,
+        }
+    }
+
+    pub fn count(&self) -> u32 {
+        (self.highest_octave - self.lowest_octave + 1).into()
+    }
+}
+
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
+enum Note {
+    C  = 0,
+    Db = 1,
+    D  = 2,
+    Eb = 3,
+    E  = 4,
+    F  = 5,
+    Gb = 6,
+    G  = 7,
+    Ab = 8,
+    A  = 9,
+    Bb = 10,
+    B  = 11,
+}
+
+struct Scale(Vec<Note>);
+
+impl Scale {
+    pub fn pentatonic() -> Self {
+        use Note::*;
+
+        Scale(vec!(
+            C,
+            D,
+            E,
+            G,
+            A,
+        ))
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
     }
 }
 
 fn midi_to_freq(note: u8) -> f32 {
     27.5 * 2f32.powf((note as f32 - 21.0) / 12.0)
-}
-
-/// Given a value, this will deterministically produce pentatonic notes from the 48-107
-/// (midi notation) range. There's 25 sounds in total before it starts looping from the beginning.
-fn pentatonic(value: u32) -> f32 {
-    let base = match value % 5 {
-        0 => 48,
-        1 => 50,
-        2 => 52,
-        3 => 55,
-        _ => 57,
-    };
-
-    let multiplier = (value / 5) % 5;
-
-    midi_to_freq(base + (multiplier * 12) as u8)
 }
 
 // For how to use this, see: https://docs.rs/thiserror/1.0.20/thiserror/
