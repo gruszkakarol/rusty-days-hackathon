2d99d6dc10bf8fc850a2d2663adce9c5ed0447ec
Sniadek (Personal) <karolgruszka@linux.home>
2020-08-02 22:41:42 +0200

    feat: build of the app

--- bootstrap.js
+++
@@ -1,5 +0,0 @@
-import "./static/scss/style.scss";
-
-import("./pkg").then((module) => {
-  module.run_app();
-});
--- src/conway/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum GameError {
-    #[error("index out of bounds")]
-    IndexOutOfBounds(crate::conway::Index),
-    #[error("game index out of bounds")]
-    GameIndexOutOfBounds(usize),
-}
